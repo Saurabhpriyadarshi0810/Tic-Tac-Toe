b5ba22bd02481379c1db4e6f0c075c4ad26b74fa
Saurabhpriyadarshi0810 <spriyadarshi831@gmail.com>
2025-07-01 23:42:31 +0530

    changed

---
+++ app.js
@@ -0,0 +1,45 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".resetbtn");
+
+let turnX = true; // turn of player O and player X
+
+const winpatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turnX) {
+      box.innerText = "X";
+      turnX = false;
+    } else {
+      box.innerText = "O";
+      turnX = true;
+    }
+    box.disabled = true;
+
+    checkwinner();
+  });
+});
+
+const checkwinner = () => {
+  for (let pattern of winpatterns) {
+    let pos1val = boxes[pattern[0]].innerText;
+    let pos2val = boxes[pattern[1]].innerText;
+    let pos3val = boxes[pattern[2]].innerText;
+
+    if (pos1val != "" && pos2val != "" && pos3val != "") {
+      if (pos1val === pos2val && pos1val === pos3val) {
+        console.log("winner is ",pos1val);
+      }
+    }
+  }
+};
